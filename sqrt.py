3e391889c26574357b35f687413f2eb5d9e4f2c1
Jason Gorman <jason.gorman@codemanship.com>
2019-10-02 08:17:51 +0100

    This is my first commit

---
+++ sqrt.py
@@ -0,0 +1,14 @@
+def sqrt(number):
+    if(number == 0):
+        return 0;
+
+    g = number/2.0;
+    g2 = g + 1;
+    while(g != g2):
+        n = number/ g;
+        g2 = g;
+        g = (g + n)/2;
+
+    return g;
+    
+print('The Square root of 25 =', sqrt(25));
